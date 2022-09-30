526f814edcafe70cef58a1fc5dc62aa4028a448a
Muriithi-Gitonga <rinkanyadavid8@gmail.com>
2022-09-30 09:23:51 +0300

    learnt

--- src/Clock.js
+++ src/Clock.js
@@ -3,13 +3,17 @@ import React, { useState, useEffect } from "react";
 function Clock() {
   const [time, setTime] = useState(new Date());
 
-  useEffect(() => {
+  const timerId = useEffect(() => {
     setInterval(() => {
       setTime(new Date());
     }, 1000);
   }, []);
 
+  return function cleanup() {
+    clearInterval(timerID);
+  }
   return <div>{time.toString()}</div>;
 }
+
 
 export default Clock;
